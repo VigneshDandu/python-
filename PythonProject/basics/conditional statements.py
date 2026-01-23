dc292e0b2353d58fa1c659ac18db11453cf4449a
Vignesh dandu <VIGNESHDANDU8355@GMAIL.COM>
2026-01-23 17:29:42 +0000

    Add additional conditional statements and examples for practice

--- PythonProject/basics/conditional statements.py
+++ PythonProject/basics/conditional statements.py
@@ -69,5 +69,45 @@
 # else: 
 #     print("Not an adult")
 #3. If the string contains “python”, print “Found”.
-print=str("Enter a string:")
-if print
+# p=str((input("Enter a string:")))
+# if p=="python" in p:
+#     print("Found")
+# else:
+#     print("Not found")
+
+#4. Check if a number is even or odd.
+# n =int(input("Enter a number: "))
+# if n%2==0:
+#     print("even number")
+# else:
+#     print("odd number")
+#5. Read a password; print “Access granted” if it matches “secret123”, else “Denied”.
+
+# password=(input("Enter password:"))
+# if password== 'secret123' :
+#     print("Access granted")
+# else:
+#     print("Denied")
+
+#6.If temperature ≥ 100, print “Boiling”, else “Not boiling”.
+
+# temprature=int(input("Enter temprature:"))
+# if temprature>=100:
+#     print("Boiling")
+# else:
+#     print("Not boiling")
+#7. Grade a score: 90–100 A, 80–89 B, 70–79 C, 60–69 D, else F.
+# score=int(input("Enter your score:"))
+# if score >=90 and score <=100:
+#     print("A grade")
+# elif score >=80 and score <=89:
+#     print("B grade")
+# elif score >=70 and score<=79:
+#     print("C grade")
+# elif  score >=60 and score<=69:
+#     print("D grade")
+# else:
+#     print("F grade")
+
+
+
